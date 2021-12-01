c71f79f7b146725e0f977a748f013a1676e95c05
Rodrigo Barroso <digorbarroso@gmail.com>
2021-11-30 21:18:16 -0300

    Todoapp 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from "axios";
-import VueAxios from "vue-axios";
-import ElementPlus from "element-plus";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import ElementPlus from 'element-plus';
 import "element-plus/dist/index.css";
 
 createApp(App)
